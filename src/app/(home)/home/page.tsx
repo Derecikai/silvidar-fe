e2280a4f4b64a19e2419e3e6612b8ca55d88ad66
Derecikai <marcu_derecichei@yahoo.com>
2025-01-20 01:54:01 +0200

    refined the home page added some ideas and saleswhatever

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -1,15 +1,43 @@
 import HomeTopButtons from "@/components/home-topButtons";
 import RealCarusel from "@/components/real-carusel";
 import { Separator } from "@/components/ui/separator";
+import { Roboto_Slab } from "@next/font/google";
+
+const roboto = Roboto_Slab({
+  subsets: ["latin"],
+  weight: ["400"], // Add weights as needed
+  style: ["normal"], // Add styles as needed
+});
 
 export default function page() {
   return (
     <div className="w-full flex flex-col bg-white min-h-screen mt-4">
-      <HomeTopButtons />
+      <div className="flex items-center w-[97%] mx-auto mt-3 lg:my-4">
+        <Separator className="flex-1 bg-main-500" />
+        <h2
+          className={`${roboto.className} lg:p-3 text-main-600 lg:text-7xl text-3xl border-main-500 rounded-lg`}
+        >
+          GHID DE UTILIZARE
+        </h2>
+        <Separator className="flex-1 bg-main-500" />
+      </div>
+      {/* <HomeTopButtons /> */}
       <RealCarusel />
-      <Separator />
-      <h2>NOUTATI</h2>
-      <Separator />
+      <div className="flex items-center w-[97%] mx-auto  mt-3 lg:my-6">
+        <Separator className="flex-1 bg-main-500" />
+        <h2
+          className={`${roboto.className} lg:p-3 text-main-600 lg:text-7xl text-3xl border-main-500 rounded-lg`}
+        >
+          POPULARE
+        </h2>
+        <Separator className="flex-1 bg-main-500" />
+      </div>
+
+      <div>asd</div>
+      <div>sad</div>
+      <div>dsaas</div>
+      <div>d</div>
+      <div>dsa</div>
     </div>
   );
 }
--- src/components/home-topButtons.tsx
+++ src/components/home-topButtons.tsx
@@ -3,9 +3,18 @@ import { Button } from "./ui/button";
 
 export default function HomeTopButtons() {
   return (
-    <div className="flex gap-2 w-[97%] mx-auto">
-      <Button>All</Button>
-      <Button variant={"secondary"}>Sterile</Button>
+    <div className="flex gap-2 xl:w-[97%]  mx-auto">
+      <Button className="h-9">All</Button>
+
+      <Button variant={"secondary"} className="bg-main-400">
+        Sterile
+      </Button>
+      <Button variant={"secondary"} className="bg-main-400">
+        Digestive
+      </Button>
+      <Button variant={"secondary"} className="bg-main-400">
+        Pisici
+      </Button>
     </div>
   );
 }
